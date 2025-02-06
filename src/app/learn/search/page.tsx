03d0823d3a0d93d60c8d342257ff4560688b1178
Jason Wu <wu80jason8@gmail.com>
2025-02-05 17:07:09 -0800

    test

--- src/app/learn/search/page.tsx
+++ src/app/learn/search/page.tsx
@@ -1,33 +1,33 @@
-"use client"
-import React, { Suspense, useEffect, useState } from 'react'
-import { useSearchParams } from 'next/navigation'
-import { useRouter } from 'next/navigation'
+"use client";
 
+import React, { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
+import { Suspense } from "react";
 
 const AIResponsePage = () => {
-    const router = useRouter();
-    const searchParams = useSearchParams();
-    const [question, setQuestion] = useState("");
-    
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const query = searchParams.get("q");
+  const [question, setQuestion] = useState("");
 
-    useEffect(() => {
-      const query = searchParams.get("q");
-      if (!query) {
-        router.push("/learn");
-        return;
-      }
-      setQuestion(query);
-    }, [searchParams, router]); //run when both router and searchParams mount/change
+  useEffect(() => {
+    if (!query) {
+      router.push("/learn");
+      return;
+    }
+    setQuestion(query);
+  }, [searchParams, router, query]); //run when both router and searchParams mount/change
 
-    // if (!question) {
-    //   return <div>Loading...</div>;
-    // }
+  // if (!question) {
+  //   return <div>Loading...</div>;
+  // }
 
-    return (
-      <Suspense fallback={<div>Loading...</div>}>
-        <div>{question}</div>
-      </Suspense>
-    );
-}
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <div>{question}</div>
+    </Suspense>
+  );
+};
 
-export default AIResponsePage
+export default AIResponsePage;
