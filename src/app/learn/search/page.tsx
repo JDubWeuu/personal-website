936a5f54ad6994ff6be2f4ed1fc0613f4e11598f
Jason Wu <wu80jason8@gmail.com>
2025-02-05 17:16:46 -0800

    please work

--- src/app/learn/search/page.tsx
+++ src/app/learn/search/page.tsx
@@ -1,33 +1,14 @@
-"use client";
-
-import React, { useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
 import { useRouter } from "next/navigation";
 import { Suspense } from "react";
+import AIResponsePage from "./search";
 
-const AIResponsePage = () => {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-  const query = searchParams.get("q");
-  const [question, setQuestion] = useState("");
-
-  useEffect(() => {
-    if (!query) {
-      router.push("/learn");
-      return;
-    }
-    setQuestion(query);
-  }, [searchParams, router, query]); //run when both router and searchParams mount/change
-
-  // if (!question) {
-  //   return <div>Loading...</div>;
-  // }
-
+const Search = () => {
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <div>{question}</div>
+      <AIResponsePage />
     </Suspense>
   );
 };
 
-export default AIResponsePage;
+export default Search;
