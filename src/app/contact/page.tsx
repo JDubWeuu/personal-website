b7113e65ea24636da9d56ba4caacc2046a99dd50
Jason Wu <wu80jason8@gmail.com>
2025-02-10 16:45:56 -0800

    form front finished

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -18,42 +18,55 @@ import {
 } from "@/components/ui/card";
 import { useForm } from "react-hook-form";
 import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
 // import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
-import { z } from 'zod'
+import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 const contactSchema = z.object({
-    name: z.string()
-        .nonempty("Name is required")
-        .refine((val) => isNaN(Number(val)), {
-            message: "Name must be a string, not a number",
-        }),
+  name: z
+    .string()
+    .nonempty("Name is required")
+    .refine((val) => isNaN(Number(val)), {
+      message: "Name must be a string, not a number",
+    }),
+  message: z
+    .string()
+    .nonempty("Message content must be included in contact form"),
 });
 
 const Contact = () => {
   const form = useForm<z.infer<typeof contactSchema>>({
     resolver: zodResolver(contactSchema),
     defaultValues: {
-        name: "",
+      name: "",
+      message: "",
     },
-    mode: "onChange"
+    mode: "onChange",
   });
 
-//   const onSubmit = (data: z.infer<typeof contactSchema>) => {
-//     console.log('Form submitted:', data);
-//   };
+  //   const onSubmit = (data: z.infer<typeof contactSchema>) => {
+  //     console.log('Form submitted:', data);
+  //   };
 
-//   const onError = (errors: any) => {
-//     console.log('Form errors:', errors);
-//   };
+  //   const onError = (errors: any) => {
+  //     console.log('Form errors:', errors);
+  //   };
+
+  const onSubmit = async (values: z.infer<typeof contactSchema>) => {
+    console.log(values);
+    // make a post request to api with values from form and send the email to my email
+  };
 
   useEffect(() => {
     console.log(form);
-  }, [form]);
+    console.log(Object.keys(form?.formState.touchedFields));
+    console.log(form?.formState?.errors);
+  }, [form.formState]);
   return (
     <div className="flex justify-center items-center w-full min-h-screen">
-      <Card className="w-[400px]">
+      <Card className="w-[420px]">
         <CardHeader>
           <CardTitle className="font-bold text-4xl">Contact</CardTitle>
           <CardDescription>
@@ -62,17 +75,20 @@ const Contact = () => {
         </CardHeader>
         <CardContent>
           <Form {...form}>
-            <form>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
               <FormField
                 control={form.control}
                 name="name"
                 render={({ field }) => {
                   return (
                     <>
-                      <FormItem>
+                      <FormItem className="mb-6">
                         <FormLabel>Name</FormLabel>
                         <FormControl>
-                          <Input placeholder="Input your first and last name" {...field} />
+                          <Input
+                            placeholder="Input your first and last name"
+                            {...field}
+                          />
                         </FormControl>
                         {form?.formState?.errors?.name && (
                           <>
@@ -80,6 +96,30 @@ const Contact = () => {
                               {form.formState.errors.name.message}
                             </FormMessage>
                           </>
+                        )}
+                      </FormItem>
+                    </>
+                  );
+                }}
+              />
+              <FormField
+                control={form.control}
+                name="message"
+                render={({ field }) => {
+                  return (
+                    <>
+                      <FormItem className="mb-6">
+                        <FormLabel>Message</FormLabel>
+                        <FormControl>
+                          <Textarea
+                            placeholder="Type in your inquiry here."
+                            {...field}
+                          />
+                        </FormControl>
+                        {form?.formState?.errors?.message && (
+                          <FormMessage>
+                            {form?.formState?.errors?.message?.message}
+                          </FormMessage>
                         )}
                       </FormItem>
                     </>
--- src/config/site.ts
+++ src/config/site.ts
@@ -1,4 +1,6 @@
-export const links = {
+import { Links } from "@/app/models/Links";
+
+export const links: Links = {
   github: "https://github.com/JDubWeuu",
   linkedin: "https://www.linkedin.com/in/jason-wu-261741215/",
 };
