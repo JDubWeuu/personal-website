b4e810ac59d4e31686c45a59d3075502511e2295
Jason Wu <wu80jason8@gmail.com>
2025-03-21 18:57:37 -0700

    linting fixes pt. 2

--- src/components/ui/link-preview.tsx
+++ src/components/ui/link-preview.tsx
@@ -67,8 +67,8 @@ export const LinkPreview = ({
 
   const translateX = useSpring(x, springConfig);
 
-  const handleMouseMove = (event: React.MouseEvent<HTMLDivElement>) => {
-    const targetRect = (event.target as HTMLElement).getBoundingClientRect();
+  const handleMouseMove = (event: React.MouseEvent<HTMLAnchorElement>) => {
+    const targetRect = (event.currentTarget as HTMLElement).getBoundingClientRect();
     const eventOffsetX = event.clientX - targetRect.left;
     const offsetFromCenter = (eventOffsetX - targetRect.width / 2) / 2; // Reduce the effect to make it subtle
     x.set(offsetFromCenter);
